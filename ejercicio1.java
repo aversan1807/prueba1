5678d859e841a8cdc22c45a11568633cad0bbfa6
aversan1807 <aversan1807@g.educaand.es>
2023-10-04 09:52:38 +0200

    version inicial

--- ejercicio1.java
+++ ejercicio1.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class ejercicio1 {
     public static void main(String[] args) {
 
-
         int a = 5;
 
         int b = 2;
 
         int c = 6;
 
-        x= ()
+        int x = ((5*2) + (6*(3-a/ 2*2)));
     }
 }
