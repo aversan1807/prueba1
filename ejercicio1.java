ccd995e90866c5b3d877c3932141bcb09436b5f1
aversan1807 <aversan1807@g.educaand.es>
2023-10-04 09:46:13 +0200

    version inicial

---
+++ ejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+
+
+        int a = 5;
+
+        int b = 2;
+
+        int c = 6;
+
+        x= ()
+    }
+}
