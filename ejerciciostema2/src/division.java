7b37c382aaa7842e328fa200d02e1d602d27f1f3
aversan1807 <aversan1807@g.educaand.es>
2023-10-17 08:30:01 +0200

    version inicial

---
+++ ejerciciostema2/src/division.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class division {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduzca el primer numero: ");
+        int numero1 = entrada.nextInt();
+        System.out.println("Introduzca segundo numero: ");
+        int numero2 = entrada.nextInt();
+
+        double x = (numero1 / numero2);
+
+        if(resultado){
+            System.out.println("El numero está bien");
+        }else(){
+
+        }
+    }
+}
---
+++ ejerciciostema2/src/ejercicio0.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ejercicio0 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numero = entrada.nextInt();
+
+        if(numero>5){
+            System.out.println("Hola mundo");
+        }else if (numero<=5){
+            System.out.println("Hola galaxia");
+    }
+    }
+}
